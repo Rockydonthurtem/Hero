1eff16d0f6b47b7b2eae3135b06be0de43fc2eb3
Rockydonthurtem <charrisq@hotmail.com>
2019-11-27 19:01:29 -0500

    Http and Edit

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,35 +1,39 @@
-// import { HEROES } from "./../mock-heroes";
-import { Hero } from "../hero";
-import { Component, OnInit } from "@angular/core";
-import { HeroService } from "../hero.service";
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
-  selector: "app-heroes",
-  templateUrl: "./heroes.component.html",
-  styleUrls: ["./heroes.component.css"]
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
-
   heroes: Hero[];
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
 
-  // getHeroes(): void {
-  //   this.heroes = this.heroService.getHeroes();
-  // }
-
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
-  // Observable to emit the array of heroes—which could happen now or several minutes from now.
-  // The subscribe() method passes the emitted array to the callback,
-  // which sets the component's heroes property.
-  // onSelect(hero: Hero): void {
-  //   this.selectedHero = hero;
-  // }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
 }
